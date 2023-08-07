5e066caf73ded9bd11ba6e178bfa955bdcb0d325
Basel <baselalnassr8@gmail.com>
2023-08-07 22:03:18 +0300

    feat: show tags next to bookmark

--- src/components/Bookmark.tsx
+++ src/components/Bookmark.tsx
@@ -1,19 +1,29 @@
 import { Linking, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Bookmark } from "../types";
-import { TouchableRipple } from "react-native-paper";
+import { TouchableRipple, useTheme } from "react-native-paper";
 
 interface BookmarkProps {
   bookmark: Bookmark;
 }
 
 export function BookmarkItem({ bookmark }: BookmarkProps) {
+  const theme = useTheme();
   const handlePress = () => Linking.openURL(bookmark.url);
 
+  const innerContainerPadding = bookmark.tag_names.length > 0 ? 10 : 0;
+
   return (
-    <View>
+    <View style={styles.container}>
       <TouchableRipple onPress={handlePress}>
-        <Text style={styles.link}>{displayBookmarkTitle(bookmark)}</Text>
+        <View
+          style={{ padding: innerContainerPadding, ...styles.innerContainer }}
+        >
+          <Text style={styles.link}>{displayBookmarkTitle(bookmark)}</Text>
+          <Text style={{ color: theme.colors.secondary }}>
+            {displayBookmarkTags(bookmark.tag_names)}
+          </Text>
+        </View>
       </TouchableRipple>
     </View>
   );
@@ -25,13 +35,21 @@ function displayBookmarkTitle(bookmark: Bookmark) {
   return bookmark.url;
 }
 
+function displayBookmarkTags(tags: string[]) {
+  const tagsWithHash = tags.map(tag => `#${tag}`);
+  return tagsWithHash.join(" ");
+}
+
 const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 5,
+  },
+  innerContainer: {
+    paddingHorizontal: 10,
+  },
   link: {
     color: "#8f9aff",
     textAlign: "left",
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderBottomWidth: 2,
-    borderColor: "#333",
+    fontWeight: "bold",
   },
 });
