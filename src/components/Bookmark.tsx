f5fdfd9a424e65bf84d52e15047a081eb665b88c
Basel <baselalnassr8@gmail.com>
2023-08-08 22:47:37 +0300

    feat: add bookmark options menu

--- src/components/Bookmark.tsx
+++ src/components/Bookmark.tsx
@@ -1,7 +1,12 @@
 import { Linking, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Bookmark } from "../types";
-import { TouchableRipple, useTheme } from "react-native-paper";
+import {
+  IconButton,
+  Menu,
+  TouchableRipple,
+  useTheme,
+} from "react-native-paper";
 
 interface BookmarkProps {
   bookmark: Bookmark;
@@ -10,6 +15,7 @@ interface BookmarkProps {
 export function BookmarkItem({ bookmark }: BookmarkProps) {
   const theme = useTheme();
   const handlePress = () => Linking.openURL(bookmark.url);
+  const [visible, setVisible] = React.useState(false);
 
   const innerContainerPadding = bookmark.tag_names.length > 0 ? 10 : 0;
 
@@ -19,20 +25,45 @@ export function BookmarkItem({ bookmark }: BookmarkProps) {
         <View
           style={{ padding: innerContainerPadding, ...styles.innerContainer }}
         >
-          <Text style={styles.link}>{displayBookmarkTitle(bookmark)}</Text>
-          <Text style={{ color: theme.colors.secondary }}>
-            {displayBookmarkTags(bookmark.tag_names)}
-          </Text>
+          <View>
+            <Text style={styles.link}>{displayBookmarkTitle(bookmark)}</Text>
+            <Text style={{ color: theme.colors.secondary }}>
+              {displayBookmarkTags(bookmark.tag_names)}
+            </Text>
+          </View>
+          <Menu
+            visible={visible}
+            onDismiss={() => setVisible(false)}
+            anchor={
+              <IconButton
+                style={styles.optionsButton}
+                icon="dots-vertical"
+                size={25}
+                onPress={() => setVisible(true)}
+              />
+            }
+          >
+            <Menu.Item title="Copy" />
+            <Menu.Item title="Edit" />
+            <Menu.Item title="Delete" />
+          </Menu>
         </View>
       </TouchableRipple>
     </View>
   );
 }
 
+const MAX_LENGTH = 40;
+
 function displayBookmarkTitle(bookmark: Bookmark) {
-  if (bookmark.title) return bookmark.title;
-  else if (bookmark?.website_title) return bookmark.website_title;
-  return bookmark.url;
+  let stringToDisplay = "";
+  if (bookmark.title) stringToDisplay = bookmark.title;
+  else if (bookmark?.website_title) stringToDisplay = bookmark.website_title;
+  else stringToDisplay = bookmark.url;
+
+  if (stringToDisplay.length > MAX_LENGTH)
+    return stringToDisplay.slice(0, MAX_LENGTH - 3) + "...";
+  return stringToDisplay;
 }
 
 function displayBookmarkTags(tags: string[]) {
@@ -45,6 +76,10 @@ const styles = StyleSheet.create({
     paddingHorizontal: 5,
   },
   innerContainer: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
     paddingHorizontal: 10,
   },
   link: {
@@ -52,4 +87,5 @@ const styles = StyleSheet.create({
     textAlign: "left",
     fontWeight: "bold",
   },
+  optionsButton: {},
 });
